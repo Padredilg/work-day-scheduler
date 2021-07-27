16691bc487b882c99251ccee3364c11349268739
Luiz P <padredilg@knights.ucf.edu>
2021-07-27 08:34:42 -0400

    finished save button and created loadTasks function

--- Develop/script.js
+++ Develop/script.js
@@ -1,21 +1,24 @@
 //VARIABLES
 var tasksArr = [];
-
 //FUNCTION DECLARATIONS
 setInterval(function(){
     var currTime = moment().format("dddd, MMM Do - hh:mm:ss A");
     $("#currentDay").text(currTime);
 },1000); 
 
-//refresh page every 10 min
-setInterval(timeRefresh(), 1000 * 60 * 10); //1000ms*60*10 === 1s*60*10 === 60s*10 === 1min*10 === 10min  
+//re-color rows every 10 min
+setTimeout(function(){
+    colorRows();
+ }, 1000 * 60 * 10); //1000ms*60*10 === 1s*60*10 === 60s*10 === 1min*10 === 10min  
 
 //assign colors to textareas depending on time of day
 var colorRows = function(){
     var presentHour = moment().hour();
+    console.log(presentHour);
 
-    $(".container").each(function(){
+    $(".row").each(function(){
         var rowHour = $(this).attr("id");
+        console.log((this).id)
         
         if(rowHour < presentHour){
             $(this).addClass("past");
@@ -31,13 +34,22 @@ var colorRows = function(){
     })
 };
 
+var loadTasks = function(){
+    
+    for(var i=0; i<9; i++){
+        tasksArr[i] = localStorage.getItem(i);
+    }
+}
 
 //FUNCTION CALLS
 colorRows();
-//loadTasks();
+loadTasks();
 
 //LISTENERS
 $(".saveBtn").on("click", function(){
     var taskDescription = $(this).siblings(".description").val();
-    var taskTime = $(this).siblings(".hour").attr("id");    
+    var taskTime = $(this).siblings(".hour").attr("id");   
+    
+    localStorage.setItem(taskTime, taskDescription);
 });
+      
