c2b1de19bf27680c0793ae2bb65a0b50944151ad
Luiz P <padredilg@knights.ucf.edu>
2021-07-27 09:39:26 -0400

    fixed logic on loadTasks and improved with for loop

--- Develop/script.js
+++ Develop/script.js
@@ -35,15 +35,14 @@ var colorRows = function(){
 };
 
 var loadTasks = function(){
-    $('#9 .description').val(localStorage.getItem('9'));
-    $('#10 .description').val(localStorage.getItem('10'));
-    $('#11 .description').val(localStorage.getItem('11'));
-    $('#12 .description').val(localStorage.getItem('12'));
-    $('#13 .description').val(localStorage.getItem('13'));
-    $('#14 .description').val(localStorage.getItem('14'));
-    $('#15 .description').val(localStorage.getItem('15'));
-    $('#16 .description').val(localStorage.getItem('16'));
-    $('#17 .description').val(localStorage.getItem('17'));
+    var description;
+    
+    for(var i=9; i<=17; i++){
+        var hour = i.toString();//hour is the string version of the value of i
+
+        description = localStorage.getItem(hour);
+        $('#'+ hour).siblings(".description").val(description);
+    }
 }
 
 //FUNCTION CALLS
