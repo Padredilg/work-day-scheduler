b2283349e9c64493cee27377c3b11d81e1fcb19a
Luiz P <padredilg@knights.ucf.edu>
2021-07-27 07:56:18 -0400

    successfuly came up with function to get the value and time of the saved task. Now have to input logic to save

--- Develop/script.js
+++ Develop/script.js
@@ -1,24 +1,7 @@
-// GIVEN I am using a daily planner to create a schedule
+//VARIABLES
 
-// WHEN I open the planner
-// THEN the current day is displayed at the top of the calendar
 
-// WHEN I scroll down
-// THEN I am presented with time blocks for standard business hours
-
-// WHEN I view the time blocks for that day
-// THEN each time block is color-coded to indicate whether it is in the past, present, or future
-
-// WHEN I click into a time block
-// THEN I can enter an event
-
-// WHEN I click the save button for that time block
-// THEN the text for that event is saved in local storage
-
-// WHEN I refresh the page
-// THEN the saved events persist
-
-//Current Date and Time
+//FUNCTION DECLARATIONS
 setInterval(function(){
     var currTime = moment().format("dddd, MMM Do - hh:mm:ss A");
     $("#currentDay").text(currTime);
@@ -42,20 +25,15 @@ var colorRows = function(){
             $(this).removeClass("present");
             $(this).addClass("future");
         }
-
-        // }
-        //make it grey
-
-        //if present hour
-        //make it red
-
-
-
-        //if future hour
-        //make it green
     })
-
-    //give each text area an id and loop through row and column checking if that hour === present hour, or past, or future
 };
 
+
+//FUNCTION CALLS
 colorRows();
+
+//LISTENERS
+$(".saveBtn").on("click", function(){
+    var taskDescription = $(this).siblings(".description").val();
+    var taskTime = $(this).siblings(".hour").attr("id");    
+});
